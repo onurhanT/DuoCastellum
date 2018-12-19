c6a566bff8e805801b83c884632260e3777f22b9
acokgunlu <cokgunluatakan@gmail.com>
2018-12-19 15:21:24 +0300

    AIController Scripts Added

---
+++ DuoCastellum/Assets/Scripts/AIController/EnemyController.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject target;
+    UnityEngine.AI.NavMeshAgent agent;
+    Animator anim;
+    public RuntimeAnimatorController death;
+    public float lookRadius = 132;
+    public int health;
+    public int damage;
+    public EnemyController target_script;
+    bool idle_con;
+    bool can_attack;
+    bool is_enemy_died;
+    bool is_dead;
+
+    // StateMachineBehaviour behaviour;
+
+    // Use this for initialization
+    void Start()
+    {
+        //behaviour = anim.GetBehaviour<KnightBehaviour>();
+        setTarget();
+        
+        target_script = target.GetComponent<EnemyController>();
+        is_dead = false;
+        idle_con = false;
+        can_attack = false;
+        is_enemy_died = false;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health > 0)
+        {
+            is_dead = false;
+            setTarget();
+            if (target == null)
+            {
+                idle_con = true; // stay at idle if there is no enemy
+                anim.applyRootMotion = false;
+                can_attack = false;
+                is_enemy_died = true;
+            }
+            else
+            {
+                is_enemy_died = false;
+                idle_con = false;
+                target_script = target.GetComponent<EnemyController>();
+                float distance = Vector3.Distance(target.transform.position, transform.position);
+                if (distance <= lookRadius)
+                {
+
+                    agent.SetDestination(target.transform.position);
+                    if (distance <= agent.stoppingDistance)
+                    {
+
+                        anim.applyRootMotion = false;
+                        can_attack = true;
+                        FaceTarget();
+                        if (target_script.health <= 0)
+                        {
+                            is_enemy_died = true;
+                            setTarget();
+                        }
+                    }
+                    else if (distance > agent.stoppingDistance)
+                    {
+                        can_attack = false;
+                        FaceTarget();
+                    }
+
+                }
+            }
+        }
+        else
+        {
+            is_dead = true;
+            gameObject.tag = "DEATH";
+            anim.runtimeAnimatorController = death;
+            Destroy(gameObject, 5);
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.transform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+    public void setTarget()
+    {
+        if (this.gameObject.tag.Equals("player1"))
+        {
+            target = GameObject.FindWithTag("player2");
+        }
+        else if (this.gameObject.tag.Equals("player2"))
+        {
+            target = GameObject.FindWithTag("player1");
+        }
+        
+    }
+
+    public bool canAttack()
+    {
+        return can_attack;
+    }
+    public bool isEnemyDied()
+    {
+        return is_enemy_died;
+    }
+
+    public void attack()
+    {
+        target_script.takeDamage(damage);
+        Debug.Log("attacked");
+    }
+
+    public void takeDamage(int damageTaken)
+    {
+        health = health - damageTaken;
+    }
+
+    public bool stayAtIdle()
+    {
+        return idle_con;
+    }
+    public bool isAtDeathState()
+    {
+        return is_dead;
+    }
+
+}
---
+++ DuoCastellum/Assets/Scripts/AIController/KnightBehaviour.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class KnightBehaviour : StateMachineBehaviour
+{
+    private EnemyController controller;
+    private Animator player;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        controller = animator.gameObject.GetComponent<EnemyController>();
+        player = animator.gameObject.GetComponent<Animator>();
+        animator.SetTrigger("idle");
+
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+     
+        if (controller.stayAtIdle())
+        {
+            player.SetBool("is_dead", true);
+            player.SetBool("run_con", true);
+            player.SetBool("on_exit", true);
+            player.SetBool("attack_con", true);
+        }
+        if (!controller.stayAtIdle())
+        {
+            player.SetTrigger("run");
+            player.SetBool("startIdle", false);
+            player.SetBool("run_con", false);
+            player.SetBool("run_con2", false);
+        }
+        if (!controller.isEnemyDied())
+        {
+            player.SetBool("is_enemy_die", true);
+            player.SetBool("is_dead", true);
+
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        controller.setTarget();
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (!controller.isAtDeathState())
+        {
+            player.SetBool("is_dead", true);
+            player.ApplyBuiltinRootMotion();
+            if (controller.canAttack())
+            {
+                //animator.applyRootMotion = false;
+                player.SetBool("on_exit", false);
+                player.SetBool("run_con", true);
+                player.SetBool("run_con2", false);
+                player.SetBool("is_enemy_die", true);
+                player.SetBool("attack_con", false);
+                player.SetBool("can_exit", true);
+                player.SetTrigger("attack");
+                if (stateInfo.IsName("wait_for_attack"))
+                {
+                    if (Time.frameCount % 15 == 0)
+                    {
+                        controller.attack();
+                    }
+                }
+                if (controller.isEnemyDied())
+                {
+                    player.SetBool("is_dead", true);
+                    player.SetBool("attack_con", true);
+                    player.SetBool("is_enemy_die", false);
+                    player.SetBool("on_exit", true);
+                    player.SetBool("can_exit", false);
+                }
+            }
+        }
+       
+    }
+
+
+}
+
+// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+//
+//}
+
