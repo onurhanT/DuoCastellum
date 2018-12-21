fad1e3a72eadc16a1e87a3ff9ed67d8036e1337f
acokgunlu <cokgunluatakan@gmail.com>
2018-12-21 12:42:29 +0300

    Singleton Script

--- DuoCastellum/Assets/Scripts/Scene Manager Scripts/DontDestroy.cs
+++ DuoCastellum/Assets/Scripts/Scene Manager Scripts/DontDestroy.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class DontDestroy : MonoBehaviour {
 
+	 private static DontDestroy _instance;
+
+    public static DontDestroy Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<DontDestroy>();
+            }
+
+            return _instance;
+        }
+    }
      void Awake()
     {
         DontDestroyOnLoad(gameObject);
